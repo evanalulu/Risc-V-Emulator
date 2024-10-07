17695cafab76c2d3ad715afe0b524362fb7f0244
Evana Pradhan <evanapra@gmail.com>
2024-10-07 02:40:42 +0000

    DON'T forget to break in switch :')

--- rv_emu.c
+++ rv_emu.c
@@ -90,6 +90,32 @@ void emu_i_arith(struct rv_state *rsp, uint32_t iw) {
      rsp->pc += 4; // Next instruction
 }
 
+void emu_i_load(struct rv_state *rsp, uint32_t iw) {
+    uint32_t rd = get_rd(iw);
+    uint32_t rs1 = get_rs1(iw);
+    uint32_t funct3 = get_funct3(iw);
+    
+    uint32_t imm_unsigned = get_bits(iw, 20, 12);
+    int64_t imm = sign_extend(imm_unsigned, 12);
+
+    uint64_t target_address = rsp->regs[rs1] + imm;
+
+    if (funct3 == 0b000) {
+        // LB
+        rsp->regs[rd] = *((uint8_t *) target_address);
+    } else if (funct3 == 0b010) {
+        // LW
+        rsp->regs[rd] = *((uint32_t *) target_address);
+    } else if (funct3 == 0b011) {
+        // LD
+        rsp->regs[rd] = *((uint64_t *) target_address);     
+    } else {
+        unsupported("I-Load-type funct3", funct3);
+    }
+
+    rsp->pc += 4; // Next instruction
+}
+
 void emu_b_type(struct rv_state *rsp, uint32_t iw) {
     uint32_t rs1 = get_rs1(iw);
     uint32_t rs2 = get_rs2(iw);
@@ -110,17 +136,85 @@ void emu_b_type(struct rv_state *rsp, uint32_t iw) {
         } else {
             rsp->pc += 4;
         }
+    } else if (funct3 == 0b101) {
+        // BGE
+        if ((int64_t)rsp->regs[rs1] >= (int64_t)rsp->regs[rs2]) {
+            rsp->pc += imm;
+        } else {
+            rsp->pc += 4;
+        }
+    } else if (funct3 == 0b000) {   // beq
+        if (rsp->regs[rs1] == rsp->regs[rs2]) {
+            rsp->pc += imm;
+        } else {
+            rsp->pc += 4;
+        } 
     } else if (funct3 == 0b001) {
         // BNE
         if ((int64_t)rsp->regs[rs1] != (int64_t)rsp-> regs[rs2]) {
             rsp->pc += imm;
         } else {
-            rsp-> pc += 4;
+            rsp->pc += 4;
         }
     } else {
         unsupported("B-type funct3", funct3);
     }
 
+}
+
+void emu_s_type(struct rv_state *rsp, uint32_t iw) {
+    uint32_t rs1 = get_rs1(iw);
+    uint32_t rs2 = get_rs2(iw);
+    uint32_t funct3 = get_funct3(iw);
+    
+    uint64_t imm11_5, imm4_0, uimm;
+    int64_t imm;
+
+    imm11_5 = get_bits(iw, 25, 7);
+    imm4_0 = get_bits(iw, 7, 5);
+    uimm = (imm11_5 << 5) | imm4_0;
+    imm = sign_extend(uimm, 12);
+
+    uint64_t target_address = rsp->regs[rs1] + imm;
+    
+    if (funct3 == 0b000) {
+        // SW
+        *((uint8_t *) target_address) = ((uint8_t)rsp->regs[rs2]);
+    } else if (funct3 == 0b010) {
+        // SW
+        *((uint32_t *) target_address) = ((uint32_t)rsp->regs[rs2]);
+    } else if (funct3 == 0b011) {  
+        // SD
+        *((uint64_t *) target_address) = ((uint64_t)rsp->regs[rs2]);
+    } else {
+        unsupported("S-type funct3", funct3);
+    }
+    
+    rsp->pc += 4;
+}
+
+void emu_j_type(struct rv_state *rsp, uint32_t iw) {
+    uint32_t rd = get_rd(iw);
+    
+    uint32_t imm_20 = get_bit(iw, 31);
+    uint32_t imm_10_1 = get_bits(iw, 21, 10);
+    uint32_t imm_11 = get_bit(iw, 20);
+    uint32_t imm_19_12 = get_bits(iw, 12, 8);
+
+    uint32_t uimm = (imm_20 << 20) | (imm_19_12 << 12) | (imm_11 << 11) | (imm_10_1 << 1);
+    int64_t imm = sign_extend(uimm, 20);
+    // printf("Immediate (imm): %ld\n", imm);
+
+    // printf("PC before JAL: %lx, rd: %u, offset: %ld\n", rsp->pc, rd, imm);
+    
+    if (rd != 0) {
+        rsp->regs[rd] = ((uint64_t)rsp->pc) + 4;
+    }
+
+    rsp->pc += imm;
+    
+    // printf("PC before JAL: %lx, rd: %u, offset: %ld\n", rsp->pc, rd, imm);
+    
 }
 
 void emu_jalr(struct rv_state *rsp, uint32_t iw) {
@@ -138,9 +232,13 @@ void emu_jalr(struct rv_state *rsp, uint32_t iw) {
 
 static void rv_one(struct rv_state *state) {
     uint32_t iw  = *((uint32_t*) state->pc);
-    //iw = cache_lookup(&state->i_cache, (uint64_t) state->pc);
+    // iw = cache_lookup(&state->i_cache, (uint64_t) state->pc);
 
     uint32_t opcode = get_opcode(iw);
+
+    // printf("iw: %08x\n", iw);
+    // printf("Opcode: %08x\n", opcode);
+
 
     switch (opcode) {
         case FMT_R:
@@ -151,9 +249,21 @@ static void rv_one(struct rv_state *state) {
             // I-type
             emu_i_arith(state, iw);
             break;
+        case FMT_I_LOAD:
+            // I-type (LD)
+            emu_i_load(state,iw);
+            break;
         case FMT_SB:
             // B-type
             emu_b_type(state, iw);
+            break;
+        case FMT_S:
+            // S-type
+            emu_s_type(state, iw);
+            break;
+        case FMT_UJ:
+            // J-type (JAL)
+            emu_j_type(state, iw);
             break;
         case FMT_I_JALR:
             // JALR (aka RET) is a variant of I-type instructions
@@ -166,12 +276,25 @@ static void rv_one(struct rv_state *state) {
 
 #if DEBUG
     printf("iw: %08x\n", iw);
+    printf("Opcode: %08x\n", opcode);
 #endif
 
 }
 
 void rv_init(struct rv_state *state, uint32_t *target, 
              uint64_t a0, uint64_t a1, uint64_t a2, uint64_t a3) {
+
+    int i;
+
+    // Zero out registers
+    for (i = 0; i < RV_NUM_REGS; i += 1) {
+        state->regs[i] = 0;
+    }
+
+    // Zero out the stack
+    for (i = 0; i < STACK_SIZE; i += 1) {
+        state->stack[i] = 0;
+    }
 
     // Initialize the Program Counter
     // rsp->pc = (uint64_t) func;
@@ -185,7 +308,7 @@ void rv_init(struct rv_state *state, uint32_t *target,
     state->regs[RV_ZERO] = 0;  // zero is always 0  (:
     state->regs[RV_RA] = RV_STOP;
     state->regs[RV_SP] = (uint64_t) &state->stack[STACK_SIZE];
-
+    
     memset(&state->analysis, 0, sizeof(struct rv_analysis_st));
     cache_init(&state->i_cache);
 }
